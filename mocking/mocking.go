9d388e611ff2d1c0b14614f6a412d805bfa497a2
SyDan0day <1293047033@qq.com>
2026-08-18 14:24:57 +0800

    mock first step done

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -4,14 +4,39 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
-func Countdown(w io.Writer) {
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type ConfiguableSleeper struct {
+	time time.Duration
+}
+
+func (o *ConfiguableSleeper) Sleep() {
+	time.Sleep(o.time)
+}
+
+func Countdown(w io.Writer, s Sleeper) {
 	for i := 3; i > 0; i-- {
+		s.Sleep()
 		fmt.Fprintf(w, "%d\n", i)
 	}
+	s.Sleep()
+	fmt.Fprint(w, "GO!")
 }
 
 func main() {
-	Countdown(os.Stdout)
+	s := &ConfiguableSleeper{1 * time.Second}
+	Countdown(os.Stdout, s)
 }
